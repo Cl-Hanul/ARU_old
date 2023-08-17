8f983237470d576be8344c9c58bf354144cdef4f
Cl-Hanul <120468442+Cl-Hanul@users.noreply.github.com>
2023-08-17 21:38:56 +0900

    토큰 파일 추가 기능

--- launch_set.py
+++ launch_set.py
@@ -1,5 +1,26 @@
-#모듈을 불러옵니다
-from os import system
+# 모듈을 불러옵니다
+import os
+from json import dump
 
-#모듈 설치
-system('pip install -r requirements.txt')
+#######################################
+
+
+# 모듈 설치
+os.system('pip install -r requirements.txt')
+
+# 키 파일 추가
+print(os.path.abspath('key.json'))
+try:
+    with open("key.json","x+") as file:
+        key = {"discord":{},
+               "naver":{},
+               "korean":{},
+               "twitch":{}}
+        key["discord"]["bot"] = {"token":""}
+        key["naver"]["api"] = {"id":"","secret":""}
+        key["korean"]["dictionary"] = {"key":""}
+        key["twitch"]["api"] = {"id":"","secret":""}
+        
+        dump(key,file,indent="\t")
+except:
+    print("\n키 파일이 확인되었습니다.\n만일 키를 작성하지 않은 경우 'key.json'에서 작성해 주세요\n")
