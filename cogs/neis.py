cf8479a37a17a7ab5648593874ed008f4984cc57
Cl-Hanul <120468442+Cl-Hanul@users.noreply.github.com>
2023-09-03 12:45:59 +0900

    dd

--- cogs/neis.py
+++ cogs/neis.py
@@ -31,23 +31,32 @@ class Neis(commands.Cog):
                 if time != now:
                     continue
                 for inform in mealList[fdcdcschool][time]:
-                    embed = ds.Embed(color=0xffd8ee,title="❗급식이 떳다!",description=f"'`{mealData.body[int(inform['mealId'])-1].school_name}`' 오늘의 급식 알림!")
-                    embed.add_field(name=mealData.body[inform['mealId']-1].meal_name.replace('조식','⛅조식').replace('중식','☀️중식').replace('석식','🌙석식'),value=f"> "+mealData.body[inform['mealId']-1].dishes.replace('\n','\n> ')+"\n```"+'\n'.join(mealData.body[inform['mealId']-1].ntr[:3])+"```")
+                    embed = ds.Embed(color=0xffd8ee,title="❗아르가 알려주는 `오늘의 메뉴`!",description=f"'`{mealData.body[int(inform['mealId'])-1].school_name}`' 오늘의 급식 알림!")
+                    embed.add_field(name=mealData.body[inform['mealId']-1].meal_name.replace('조식','⛅아침밥').replace('중식','☀️점심밥').replace('석식','🌙저녁밥'),value=f"> "+mealData.body[inform['mealId']-1].dishes.replace('\n','\n> ')+"\n```"+'\n'.join(mealData.body[inform['mealId']-1].ntr[:3])+"```")
                     try:
                         await self.bot.get_channel(inform["channelId"]).send(embed=embed)
                     except Exception as e:
                         print(e)
 
-    @meal.command(name="학교설정",description="급식 명령어 이용을 위해 학교 정보를 입력합니다.")
+    @meal.command(name="학교설정",description="아르가 학교를 참고할 수 있게 해주는 명령어!")
     @app_commands.describe()
     async def meal_setting_school(self,interaction:ds.Interaction,school_name:str):
+        if len(school_name) < 2:
+            embed = ds.Embed(color=0xffaa66,title="학교 이름은 2글자 이상으로 입력해줘!",description="정확히 입력해줘야 돼!")
+            await interaction.response.send_message(embed=embed)
+            return
+        if len(school_name) > 24:
+            embed = ds.Embed(color=0xffaa66,title="학교 이름은 24글자 이하로 입력해줘!",description="정확히 입력해줘야 돼!")
+            await interaction.response.send_message(embed=embed)
+            return
         schoolData = get_school_by_name(school_name)
         if not schoolData:
-            await interaction.response.send_message("학교 못찾음")
+            embed = ds.Embed(color=0xee6666,title="어.. 아르가 찾아 봤는데, 그런 학교는 없는 것 같아!",description="띄어쓰기나 철자를 다시 확인해줘!")
+            await interaction.response.send_message(embed=embed)
             return
-        
+
         view = View()
-        selection = Select(options=[ds.SelectOption(label=school["name"],value=f'{school["ofcdc_code"]}{school["code"]}',description=school["pos"]) for school in schoolData])
+        selection = Select(placeholder="아르가 열심히 찾아왔으니 선택해줘!", options=[ds.SelectOption(label=school["name"],value=f'{school["ofcdc_code"]}{school["code"]}',description=school["pos"]) for school in schoolData])
         schoolnames = {}
         for schoolSelectOption in selection.options:
             schoolnames[schoolSelectOption.value] = schoolSelectOption.label
@@ -55,37 +64,47 @@ class Neis(commands.Cog):
         async def selection_callback(interaction:ds.Interaction):
             with open('data\\neis.json') as file:
                 userSchool = json.load(file)
-            if str(interaction.user.id) not in userSchool["school"]:
+            if str(interaction.user.id) in userSchool["school"]:
                 userSchool["school"][str(interaction.user.id)] = selection.values[0]
-                await interaction.response.send_message(f"`{schoolnames[selection.values[0]]}`로 학교 설정을 이전합니다.",ephemeral=True)
-                if selection.values not in userSchool["meal"]:
+                embed = ds.Embed(color=0xffd8ee,title=f"사실 `{schoolnames[selection.values[0]]}` 였구나!",description="이제 아르는 그렇게 알고 있을게!")
+                await interaction.response.send_message(embed=embed,ephemeral=True)
+                if selection.values[0] not in userSchool["meal"]:
                     userSchool["meal"][selection.values] = {}
-            elif str(interaction.user.id) == userSchool["school"][str(interaction.user.id)]:
-                await interaction.response.send_message("이미 그 학교로 설정되있음.")
+            elif selection.values[0] == userSchool["school"][str(interaction.user.id)]:
+                embed = ds.Embed(color=0x66ff66,title="아르한테 이미 그 학교로 알려줬어!",description="적용은 잘 되있어!")
+                await interaction.response.send_message(embed=embed,ephemeral=True)
             else:
                 userSchool["school"][str(interaction.user.id)] = selection.values[0]
-                await interaction.response.send_message(f"이제부터 설정된 학교는 `{schoolnames[selection.values[0]]}` 입니다.",ephemeral=True)
-                if selection.values not in userSchool["meal"]:
-                    userSchool["meal"][selection.values] = {}
+                embed = ds.Embed(color=0xffd8ee,title="알려줘서 고마워!",description=f"아르는 이제 `{schoolnames[selection.values[0]]}`로 알고 있을게!")
+                await interaction.response.send_message(embed=embed,ephemeral=True)
+                if selection.values[0] not in userSchool["meal"]:
+                    userSchool["meal"][selection.values[0]] = {}
             with open('data\\neis.json',"w") as file:
                 json.dump(userSchool,file)
         selection.callback = selection_callback
         view.add_item(selection)
         await interaction.response.send_message(view=view,ephemeral=True)
     
-    @meal.command(name="알림추가",description="매주 설정 시간에 급식 알림을 이 채널에 추가합니다.")
-    @app_commands.describe(alert_time="24시간 형식의 '시시분분' 형식으로 입력합니다. 예)2104")
-    @app_commands.describe(meal_id="알림 받을 급식을 정합니다(조식,중식,석식)")
+    @meal.command(name="알림추가",description="아르가 그 날에 급식을 알려줘!")
+    @app_commands.describe(alert_time="24시간 형식의 '시시분분' 예)2104")
+    @app_commands.describe(meal_id="알림 받을 급식 (아침,점심,저녁)")
     @app_commands.choices(meal_id=[
-        app_commands.Choice(name="조식", value="1"),
-        app_commands.Choice(name="중식", value="2"),
-        app_commands.Choice(name="석식", value="3"),
+        app_commands.Choice(name="아침", value="1"),
+        app_commands.Choice(name="점심", value="2"),
+        app_commands.Choice(name="저녁", value="3"),
     ])
     async def add_meal_alert(self,interaction:ds.Interaction,alert_time:str,meal_id:app_commands.Choice[str]):
+        if int(alert_time[:2]) == 24:
+            alert_time = "00" + alert_time[2:4]
+        if int(alert_time[:2]) < 0 | int(alert_time[2:4]) > 59 | int(alert_time[2:4]) < 0:
+            embed = ds.Embed(color=0xffaa66,title="존재하는 시간으로 입력해줘!",description="아르의 시계는 23시랑 59분을 넘어가는 시계가 없어!")
+            await interaction.response.send_message(embed=embed)
+            return
         with open('data\\neis.json') as file:
             neisData = json.load(file)
         if str(interaction.user.id) not in neisData["school"]:
-            await interaction.response.send_message("`/급식 학교설정` 명령어를 먼저 이용해 주세요")
+            embed = ds.Embed(color=0xffaa66,title="`/급식 학교설정`로 아르한테 학교를 먼저 알려줘!",description="그래야 아르가 급식을 알려줄 수 있어!")
+            await interaction.response.send_message(embed=embed)
             return
         code_all = neisData["school"][str(interaction.user.id)]
         if alert_time not in neisData["meal"][code_all]:
@@ -93,31 +112,32 @@ class Neis(commands.Cog):
         neisData["meal"][code_all][alert_time].append({"channelId": interaction.channel.id, "mealId": meal_id.value})
         with open('data\\neis.json',"w") as file:
             json.dump(neisData,file)
-        await interaction.response.send_message(f"이제부터 매 주 '`{alert_time[:2]}시 {alert_time[2:]}분`'에 '`{meal_id.name}`'을 이 채널로 알려줍니다.")
+        embed = ds.Embed(color=0xffd8ee,title=f"'`{alert_time[:2]}시 {alert_time[2:]}분`'에 '`{meal_id.name}`' 접수완료~!",description=f"이제부터 아르가 매 주 `{meal_id.name}`을 알려줄게!")
+        await interaction.response.send_message(embed=embed)
         
 
     
     
-    @meal.command(name="정보",description="학교의 급식을 알려줍니다.")
-    @app_commands.describe(school_name="학교 이름 예)'00중학교'")
-    @app_commands.describe(meal_date="급식 날짜 예)20230124")
+    @meal.command(name="정보",description="아르가 학교의 급식을 알려줘!")
+    @app_commands.describe(school_name="학교 이름! 예)'00중학교'")
+    @app_commands.describe(meal_date="급식 날짜! 예)20230124")
     async def meal_info(self,interaction:ds.Interaction,school_name:str,meal_date:int):
         school = get_school_by_name(school_name)
         
         if not school:
-            await interaction.response.send_message("학교 못찾음")
+            embed = ds.Embed(color=0xee6666,title="어.. 아르가 찾아 봤는데, 그런 학교는 없는 것 같아!",description="띄어쓰기나 철자를 다시 확인해줘!")
+            await interaction.response.send_message(embed=embed)
             return
         
         meal = get_meal(school[0]['ofcdc_code'],school[0]['code'],meal_date)
         if not meal.body:
-            await interaction.response.send_message("급식 없음")
+            embed = ds.Embed(color=0xff48ee,title="급식이 없어..",description="큰일이야..!!")
+            await interaction.response.send_message(embed=embed)
             return
         
-        embed = ds.Embed(color=0xffd8ee, title=f"{meal.body[0].school_name}",description=f"**'{str(meal_date)[:4]}년 {str(meal_date)[4:6]}월 {str(meal_date)[6:]}일'** 급식!")
+        embed = ds.Embed(color=0xffd8ee, title=f"{meal.body[0].school_name}",description=f"아르가 알려주는 **'{str(meal_date)[:4]}년 {str(meal_date)[4:6]}월 {str(meal_date)[6:]}일'** 급식!")
         embed.add_field(name="---",value="",inline=False)
         for m in meal.body:
-            embed.add_field(name=f"{m.meal_name.replace('조식','⛅조식').replace('중식','☀️중식').replace('석식','🌙석식')} ({m.calories})",value=f"> "+m.dishes.replace('\n','\n> ')+"\n```"+'\n'.join(m.ntr[:3])+"```")
+            embed.add_field(name=f"{m.meal_name.replace('조식','⛅아침밥').replace('중식','☀️점심밥').replace('석식','🌙저녁밥')} ({m.calories})",value=f"> "+m.dishes.replace('\n','\n> ')+"\n```"+'\n'.join(m.ntr[:3])+"```")
         
         await interaction.response.send_message(embed=embed)
-        
-        
