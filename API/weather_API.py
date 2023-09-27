d6c8331e39d9ecd87627b608ed535303a0aad0bd
Cl-Hanul <120468442+Cl-Hanul@users.noreply.github.com>
2023-09-28 01:03:03 +0900

    ``

---
+++ API/weather_API.py
@@ -0,0 +1,28 @@
+import requests as req
+from datetime import datetime
+from json import load
+
+with open("V2\\API\\key.json","r",encoding="utf-8") as file:
+    readFile = load(file)
+    client_key = readFile["gi-sang"]["dan-gi"]["key"]
+
+class dictData:
+    class _Body:
+        def __init__(self, date:str,items:list) -> None:
+            self.date = date
+            self.items = items
+
+    def __init__(self, resCode:int = -1,body:None|_Body = None,length = 0) -> None:
+        self.resCode = resCode
+        self.body = body
+        self.length = length
+
+def getWeather(datetime:str,x,y) -> dict:
+    url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst'
+    params ={'serviceKey' : client_key, 'pageNo' : '1', 'numOfRows' : '1000', 'dataType' : 'JSON', 'base_date' : f'{datetime}', 'base_time' : '0600', 'nx' : f"{x}", 'ny' : f"{y}" }
+
+    response = req.get(url, params=params)
+    return response.content
+
+if __name__ == "__main__":
+    print(getWeather(datetime.strftime(datetime.now(),"%Y%m%d"),55,100))
